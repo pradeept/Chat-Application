fceb45b1e4331c721a43ce07e9d2d999603fd600
pradeept <pradeep.tarakar17@gmail.com>
2023-06-08 23:14:35 +0530

    working room-chat

--- index.mjs
+++ index.mjs
@@ -4,38 +4,58 @@ import fs from 'fs';
 
 
 const httpServer = http.createServer((req,res)=>{
-  console.log(req.url);
+
   if(req.url === '/'){
     res.writeHead(200,{"Content-Type": "text/html"});
     const html = fs.readFileSync("./index.html");
     res.end(html);
+  }else if(req.url.match('Room') !== null){
+    res.writeHead(200,{"ContentType":"text/html"});
+    const html = fs.readFileSync("./chat.html");
+    res.end(html);
   }else if(req.url.match("\.css$")){
-    console.log(req.url);
+    
     var cssPath = "./public/"+req.url;
     var fileStream = fs.createReadStream(cssPath, "UTF-8");
     res.writeHead(200, {"Content-Type": "text/css"});
     fileStream.pipe(res);
 }else if(req.url.match("\.js$")){
-    var imagePath = "./public/"+req.url;
-    var fileStream = fs.createReadStream(imagePath);
+    var scriptPath = "./public/"+req.url;
+    var fileStream = fs.createReadStream(scriptPath);
     res.writeHead(200, {"Content-Type": "text/javascript"});
     fileStream.pipe(res);
+}else if(req.url.match(".\png$")){
+  var imagePath = '.'+req.url;
+  var fileStream = fs.createReadStream(imagePath);
+  res.writeHead(200, {"Content-Type": "image/png"});
+  fileStream.pipe(res);
 }else{
     res.writeHead(404, {"Content-Type": "text/html"});
     res.end("No Page Found");
 }
 });
 
-
 const io = new Server(httpServer,{});
+var usersOnline = [];
 io.on("connection",(socket)=>{
   console.log("Connection recieved!");
   console.log(socket.id);
-  
-  socket.on("Clicked",(msg,name,time)=>{
-    socket.broadcast.emit('broadcast',msg,name,time);
+
+  setInterval(()=>{
+    socket.emit("user-online",usersOnline)
+  }, 1000);
+
+  socket.on('join-room',async(room,name)=>{
+    await socket.join(room);
+    usersOnline.includes(name)?null:usersOnline.push(name);
+    console.log(usersOnline);
+    socket.to(room).emit('user-online',usersOnline);
   });
-  
+
+  socket.on("Clicked",(msg,name,time,room)=>{
+    socket.to(room).emit('broadcast',msg,name,time);
+    socket.to(room).emit('user-online',usersOnline);
+  }); 
 });
 
 
--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 $(document).ready(()=>{
     var name = ""
+    var room = ""
+    var onlineUsers = [];
     $('.chat-send').click(async ()=>{
         const val = $('.user-input').val();
         const el = `<p class='sender-chat'><span>${val}</span></p>`
@@ -8,20 +10,56 @@ $(document).ready(()=>{
         
         // emit msg event
         const time = new Date().toLocaleTimeString(); 
-        await socket.emit('Clicked',val,name,time);
+        await socket.emit('Clicked',val,name,time,room);
         $('.user-input').val("");
     });
+
+    // Find Parametes from URL
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+    
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+    
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+        return false;
+    };
 
     var socket = io();
    
     socket.on("connect", () => {
-        name = prompt('Enter your name');
-        $('.user-title').text(name);
-        console.log("Connected ID: "+socket.id); // x8WIv7-mJelg7on_ALbx
+        name = getUrlParameter('name');
+        room = getUrlParameter('room');
+
+        $('.user-title').text(name);        //set user name in chat UI
+        socket.emit('join-room',room,name);
+        
+    });
+
+    socket.on('user-online',(usersOnline)=>{
+        console.log(usersOnline);
+        if(onlineUsers.sort().join(',')=== usersOnline.sort().join(',')){
+           return;
+        }else{
+            $('.add-online-users').empty();
+            onlineUsers = usersOnline;
+            for(let item of onlineUsers){
+                console.log(item);
+                let ele = `<div class="online-users-list"><i class="fa-solid fa-user user-icon"></i> ${item}</div>`
+                $('.add-online-users').append(ele);
+            }
+        }
+       
     });
 
     socket.on("disconnect", () => {
-        console.log("Disconnected!"); // undefined
+        console.log("Disconnected!"); 
     });
 
     
@@ -31,10 +69,9 @@ $(document).ready(()=>{
                 <span>${msg}</span>\
                 <p class='user-name'>${name+"\n"+time}</p>\
                 </div>`
-        // let el =`<p class='receiver-chat'><span>${msg}</span></p>`
         $('.msg-container').append(el);
         $(".msg-container").animate({ scrollTop: 20000000 },100);
+      
+        
     });
-
-    
 });
